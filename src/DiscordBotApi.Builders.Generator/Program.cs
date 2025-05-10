c65fc5e3d18571b9b89751987aa754d0191e75a9
martin <martin.bergablod@outlook.com>
2025-05-10 09:21:44 +0200

    Update builders

--- src/DiscordBotApi.Builders.Generator/Program.cs
+++ src/DiscordBotApi.Builders.Generator/Program.cs
@@ -1,3 +1,9 @@
-﻿using DiscordBotApi.Builders.Generator;
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="kpop.fan">
+//   Copyright (c) 2025 kpop.fan. All rights reserved.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+using DiscordBotApi.Builders.Generator;
 
 new SourceCodeGenerator().Main();
